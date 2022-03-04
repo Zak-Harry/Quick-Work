d81f7fd0152af6dcb08d6b38e0c5b8954567f766
Zak-Harry <azdidonato@gmail.com>
2022-03-04 15:53:32 +0100

    planning form_creation and edit

--- src/Security/Voter/ProfilVoter.php
+++ src/Security/Voter/ProfilVoter.php
@@ -58,7 +58,7 @@ class ProfilVoter extends Voter
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
             case self::EDIT:
-                if($subject->getviewData()->getid() === $user->getid()){ return true;};
+                if($subject->getviewData()->getid() === $user->getd()){ return true;};
                 break;
             case self::CREATE:
                 if($this->security->isGranted('ROLE_RH')){ return true;};
