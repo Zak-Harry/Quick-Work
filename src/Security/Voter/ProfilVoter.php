0034760220d6f401b7e14550fe78741e5fd6c908
carlialex13 <carli.alex.13@gmail.com>
2022-03-04 12:52:06 +0100

    Fix : EDIT & CREATE with RH ROLE

--- src/Security/Voter/ProfilVoter.php
+++ src/Security/Voter/ProfilVoter.php
@@ -58,10 +58,10 @@ class ProfilVoter extends Voter
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
             case self::EDIT:
-                if($subject->getviewData()->getid() === $user->getid()){ return true;};
+                if($subject->getviewData()->getid() === $user->getid() || $this->security->isGranted("ROLE_RH")){ return true;};
                 break;
             case self::CREATE:
-                if($this->security->isGranted('ROLE_RH')){ return true;};
+                if($this->security->isGranted("ROLE_RH")){ return true;};
                 break;
             case self::VIEW:
                 if($this->security->isGranted('ROLE_USER')){ return true;};
