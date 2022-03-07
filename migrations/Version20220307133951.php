dabe9dc22df46fba00951f28bfdc690291d591ef
Zak-Harry <azdidonato@gmail.com>
2022-03-07 14:55:20 +0100

    fix entity bug typage

--- migrations/Version20220307133951.php
+++ migrations/Version20220307133951.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220307095046 extends AbstractMigration
+final class Version20220307133951 extends AbstractMigration
 {
     public function getDescription(): string
     {
