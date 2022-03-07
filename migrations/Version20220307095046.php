8b29e934eed40cb53fd0fe193a05ed4554ef59d7
Lyriane <lynda.lgg3@gmail.com>
2022-03-07 11:50:54 +0100

    upgrade view planning with dynamization

--- migrations/Version20220307095046.php
+++ migrations/Version20220307095046.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220304132227 extends AbstractMigration
+final class Version20220307095046 extends AbstractMigration
 {
     public function getDescription(): string
     {
