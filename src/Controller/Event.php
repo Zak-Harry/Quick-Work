f98c77772e83f8e3cdbe0197a569d0d8473ed09e
carlialex13 <carli.alex.13@gmail.com>
2022-03-16 20:06:39 +0100

    Feat : Add Form in template event/index.html.twig

--- src/Controller/Event.php
+++ src/Controller/Event.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\EventType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +15,13 @@ class Event extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('/event/index.html.twig');
+        $eventForm = $this->createForm(EventType::class);
+
+        return $this->renderForm('/event/index.html.twig',
+            [
+            'event' => $eventForm
+            ]
+        );
     }
 
 }
--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -3,7 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Event;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +18,32 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('type')
-            ->add('startEvent')
-            ->add('endEvent')
-            ->add('durationEvent')
-            ->add('status')
-            ->add('message')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('user')
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Congé' => 1,
+                    'Formation' => 2,
+                    'Maladie' => 3
+                ]
+            ])
+            ->add('startEvent', DateTimeType::class, [
+
+            ])
+            ->add('endEvent', DateTimeType::class, [
+
+            ])
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'En cours' => 1,
+                    'Validé' => 2,
+                    'Annulé' => 3,
+                    'Rejeté' =>4
+                ]
+            ])
+            ->add('message', TextType::class)
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'firstname',
+            ])
         ;
     }
 
